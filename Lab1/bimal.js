68dba639f49d3855d3af49630a55eea6d7a15f26
bimal-tech <074bct107@nce.edu.np>
2023-03-11 13:01:34 +0545

    Basic understanding of lab1

---
+++ Lab1/bimal.js
@@ -0,0 +1,10 @@
+// Program to find the circumference of circle
+/*
+Formula : 2* pi *r;
+*/
+
+const pi = 3.14
+var radius = 4;
+var circumference = 0;
+circumference = 2 * pi * radius;
+console.log(circumference);
