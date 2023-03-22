7e07a545d38fa65880ddfa73f1693c0305e29731
bimal-tech <074bct107@nce.edu.np>
2023-03-22 20:23:19 +0545

    lab 3 and lab 4 uploaded

---
+++ Lab3/domready.js
@@ -0,0 +1,26 @@
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(generateButton);
+var generateButton = document.getElementsByClassName("save-class");
+console.log(generateButton);
+var generateButton = document.querySelector(".save-class");
+console.log(generateButton);
+var generateButton = document.querySelector("#save-id");
+console.log(generateButton);
+var generateButton = document.querySelector("button");
+console.log(generateButton);
